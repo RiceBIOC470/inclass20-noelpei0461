98e096c2ba1308127e213efd4a559d8d6396d638
noelpei0461 <31324311+noelpei0461@users.noreply.github.com>
2017-11-09 13:37:05 -0600

    Add files via upload

--- Inclass20.m
+++ Inclass20.m
@@ -7,9 +7,59 @@
 
 % 1. Define a custom model and try to fit the data to a Michaelis function 
 % y = V*x/(K+x). 
+file = load('data.mat');
+x = file.xx;
+y = file.yy;
+
+fit1 = 'V*x/(K+x)';
+model = fittype(fit1);
+[fit_out,fit_metric] = fit(x',y',model);
+figure
+scatter(x,y)
+hold on
+plot(x',fit_metric)
 % 2. Use a custom model to try to fit the data to a Hill function with
 % exponent 2: y = V*x^2/(K^2+x^2)
+file = load('data.mat');
+x = file.xx;
+y = file.yy;
+
+fit2 = 'V*x^2/(K^2+x^2)';
+model2 = fittype(fit2);
+[fit_out,fit_metric] = fit(x',y',model2);
+figure
+scatter(x,y)
+hold on
+plot(x',fit_metric)
+
 % 3. Now treat the exponent as a free parameter and perform the fit again,
 % that is fit to the model y = V*x^n/(K^n+x^n)
+file = load('data.mat');
+x = file.xx;
+y = file.yy;
+
+fit3 = 'V*x^n/(K^2+x^2)';
+model3 = fittype(fit3);
+[fit_out,fit_metric] = fit(x',y',model3);
+figure
+scatter(x,y)
+hold on
+plot(x',fit_metric)
+
 % 4. Redo parts 1 and 2, but using your model from 3 and fixing the value of
 % n to either 1 or 2.
+%n=1
+model4 = fittype(fit3,'problem',n);
+[fit_out,fit_metric] = fit(x',y',model4,'problem',1);
+figure
+scatter(x,y)
+hold on
+plot(x',fit_metric)
+%n=2
+model4 = fittype(fit3,'problem',n);
+[fit_out,fit_metric] = fit(x',y',model4,'problem',2);
+figure
+scatter(x,y)
+hold on
+plot(x',fit_metric)
+
